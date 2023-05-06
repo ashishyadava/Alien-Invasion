f64eb844d86f0d73ae79c1b4ef16af5df2bd6696
Ashish <ashishyadava1996@gmail.com>
2023-05-06 13:38:39 +0530

    Added basic fremework

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,3 +16,15 @@ class AlienInvasion:
     def run_game(self):
 
         while True:
+
+            for event in pygame.event.get()
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+
+    if __name__ == '__main__':
+
+        ai = AlienInvasion()
+        ai.run_game()
+
