f31d426f63740073f16fb16490abb495849ea6b0
Ashish <ashishyadava1996@gmail.com>
2023-05-07 11:32:39 +0530

    Added left and right movements for ship

--- ship.py
+++ ship.py
@@ -18,6 +18,18 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+
+        if self.moving_right:
+            self.rect.x += 1
+
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
